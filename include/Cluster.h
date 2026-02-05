f806226ca9f08b5742ecc56b8f2e838796529162
Anand S Nair <76837819+AnandSNair123@users.noreply.github.com>
2026-02-05 18:01:27 +0530

    Update Cluster.h

--- include/Cluster.h
+++ include/Cluster.h
@@ -4,29 +4,46 @@
 #include "Environment.h"
 #include "Species.h"
 #include <string>
-#include <vector>
-#include <utility> 
 
-class Cluster : public Environment
+class Cluster : public Environment, protected Bacterium
 {
-protected:
-    std::vector<Bacterium> alive;
-    std::vector<Bacterium> dead;
 
+protected:
+    // A vector of bacterium which contains all alive members
+    vector<Bacterium> alive = {};
+    // A vector of bacterium which contains all dead members
+    vector<Bacterium> dead = {};
+
+    // values containing total values of bacteria
     unsigned long int totalBacteria = 0;
     unsigned long int totalAliveBacteria = 0;
     unsigned long int totalDeadBacteria = 0;
     
-    std::pair<bool, unsigned long int> isPresent(const Bacterium& b);
-    void add(Bacterium* b);
-    void omit(Bacterium* b);
+    // Accessors 
+    std::pair<bool, unsigned long int> isPresent( Bacterium );
+    
+    // Mutators - functions to edit the members of the cluster
+    // add - registers the bacterium into the cluster
+    void add( Bacterium* );
+    void omit( Bacterium* );
+
     void step();
 
+
 public:
-    Cluster(int numBacteria = 100, int randomiseType = 1, double EnergyLevel = 300.0f);
+
+    // initializer
+    Cluster(int numBacteria = 100, int randomiseType = 1, 
+            double EnergyLevel = 300.0f);
 
     void updateTemporalResolution(double tempRes);
+
+    // runs as long as all the bacteria does not die
     void run(std::string filename);
+    // runs uptil a particular time speciefied or until all bacteia die
+    void run(std::string filename, double time);  
+
 };
+
 
 #endif
