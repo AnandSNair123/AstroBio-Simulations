b18813e9d62f11042cfb472a8df171f587574f41
Anand S Nair <76837819+AnandSNair123@users.noreply.github.com>
2026-02-05 18:01:02 +0530

    Refactor main.cpp to include error handling
    Added error handling for Python script execution.

--- apps/main.cpp
+++ apps/main.cpp
@@ -2,22 +2,24 @@
 #include <string>
 #include <iostream>
 #include "Cluster.h"
-
 using namespace std;
 
 int main()
 {
+    // Output filename
     string filename = "trial1.csv";
 
-    cout << "Initializing Base Simulation..." << endl;
-    
-    // Start with 1000 bacteria (Clean Slate)
-    Cluster cottonBed(1000);             
-    
-    cottonBed.run(filename);    
+    // Initialising environment and running simulations
+    Cluster cottonBed(100);             // initial number of bacteria
+    cottonBed.run(filename);    // inputs -name of output file
 
-    string command = "python ../utils/plot.py " + filename;
+    // Calling python script to plot graph
+    string command = "../utils/plot.py " + filename;
     int return_code = system(command.c_str());
-    
+    if (return_code != 0){
+        cout << "Python Code failed with exit code : " << return_code;
+        return return_code + 10;
+    }
+
     return 0;
 }
